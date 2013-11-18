4e0e7d4a8319ecac7f01b4e0c5cb62e10524ea1f
Tiberiu Chibici <chibicitiberiu@gmail.com>
2013-11-18 20:03:00 +0200

    Build 121209

---
+++ DrumKit/Recorder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrumKit
+{
+    class Recorder
+    {
+        private SortedDictionary<double, Uri> hits;
+        private DateTime begin;
+
+        public bool IsRecording { get; private set; }
+
+        public Recorder()
+        {
+            this.IsRecording = false;
+            this.begin = new DateTime();
+            this.hits = new SortedDictionary<double, Uri>();
+        }
+
+        public void Start()
+        {
+            this.IsRecording = true;
+            begin = DateTime.Now;
+        }
+
+        public void Add(Uri uri)
+        {
+            if (!IsRecording)
+                return;
+
+            var time = DateTime.Now - this.begin;
+            this.hits.Add(time.TotalMilliseconds, uri);
+        }
+
+        public void Stop()
+        {
+            this.IsRecording = false;
+        }
+
+        public void Play()
+        {
+            SoundPlayer player = new SoundPlayer();
+
+            var sounduris = this.hits.Values.Distinct();
+            foreach (var i in sounduris)
+                player.AddWave(i.AbsolutePath, i.AbsolutePath);
+
+
+        }
+    }
+
+}
---
+++ DrumKit/SoundPlayer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.XAudio2;
+using SharpDX.IO;
+using SharpDX.Multimedia;
+
+namespace DrumKit
+{
+    class SoundPlayer
+    {
+        private XAudio2 xaudio;
+        private MasteringVoice mvoice;
+        Dictionary<string, MyWave> sounds;
+        SoundPool pool;
+
+        public SoundPlayer()
+        {
+            xaudio = new XAudio2();
+            xaudio.StartEngine();
+            mvoice = new MasteringVoice(xaudio);
+            sounds = new Dictionary<string, MyWave>();
+        }
+
+        public void AddWave(string key, string filepath)
+        {
+            MyWave wave = new MyWave();
+
+            var nativeFileStream = new NativeFileStream(filepath, NativeFileMode.Open, NativeFileAccess.Read, NativeFileShare.Read);
+            var soundStream = new SoundStream(nativeFileStream);
+            var buffer = new AudioBuffer() { Stream = soundStream, AudioBytes = (int)soundStream.Length, Flags = BufferFlags.EndOfStream };
+
+            wave.Buffer = buffer;
+            wave.DecodedPacketsInfo = soundStream.DecodedPacketsInfo;
+            wave.WaveFormat = soundStream.Format;
+
+            this.sounds.Add(key, wave);
+        }
+
+        public void AddDrums(IEnumerable<Drum> drums)
+        {
+            foreach (var d in drums)
+                foreach (var s in d.SoundSources)
+                {
+                    string key = d.Name + s.Key.ToString();
+                    string path = s.Value.AbsolutePath.TrimStart('\\', '/');
+                    this.AddWave(key, path);
+                }
+        }
+
+        public void Play(string key)
+        {
+            if (!this.sounds.ContainsKey(key)) return;
+            MyWave w = this.sounds[key];
+
+            if (pool == null)
+                pool = new SoundPool(xaudio, w.WaveFormat);
+
+            pool.PlayBuffer(w.Buffer, w.DecodedPacketsInfo);
+        }
+    }
+
+    class MyWave
+    {
+        public AudioBuffer Buffer { get; set; }
+        public uint[] DecodedPacketsInfo { get; set; }
+        public WaveFormat WaveFormat { get; set; }
+    }
+}
